99cb46e6215ffb10e884393ec694fa93b6337155
JDLanata <87950670+JDLanata@users.noreply.github.com>
2021-10-20 15:37:53 -0700

    created notes route, renamed db json file to notes.json

---
+++ routes/notes.js
@@ -0,0 +1,60 @@
+const notes = require('express').Router();
+const path = require('path');
+// const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+const db = fs.readFileSync('./db/notes.json','utf-8')
+
+
+
+// GET Route for retrieving notes information
+// notes.get('/', (req, res) => {
+//   // TODO: Logic for sending all the content of db/notes.json
+// fs.readFile('./db/notes.json').then((data)=> res.json(JSON.parse(data)));
+
+// });
+
+notes.get('/notes', (req, res) =>{
+
+
+    console.log(db);
+    res.json(JSON.parse(db))
+}
+
+);
+
+// POST Route for a error logging
+notes.post('/notes', (req, res) => {
+  // TODO: Logic for appending data to the db/notes.json file
+  const { title, text } = req.body;
+
+
+  if (req.body) {
+    const newNotes = {
+      title,
+      text,
+    };
+   db.push(newNotes)
+
+    fs.writeFile('./db/notes.json', JSON.stringify(db), (err) =>
+    err ? console.error(err):console.log('not saved')
+    
+    );
+  
+    res.json("notes saved");
+  } 
+
+});
+
+
+
+
+    // // Convert the data to a string so we can save it
+    // reviews.push(newReview)
+    // const reviewString = JSON.stringify(reviews,null,4);
+
+  
+   
+
+
+
+module.exports = notes;
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const path = require('path')
+// const api = require('./assets/js/index.js');
+const notes = require('./routes/notes')
+
+const PORT = process.env.port || 3001;
+
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/notes',function(req,res){res.sendFile(__dirname,'./notes.html')})
+
+app.use('/api', notes);
+
+
+
+
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+
+//GET wildcard route.
+app.get('/*', (req,res) => 
+
+  res.sendFile(path.join(__dirname,'/public/notes.html'))
+
+);
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
